5284a506aab1bcff0029da9bd184e7897928c03c
reviewnextzen-lang <reviewnextzen@gmail.com>
2026-05-01 18:21:28 +0600

    Add files via upload

---
+++ blogposthome.js
@@ -0,0 +1,66 @@
+let posts = [];
+
+async function loadPosts(){
+
+  const feed = location.origin + "/feeds/posts/default/-/Blogpost?alt=json&max-results=50";
+
+  try{
+    const res = await fetch(feed);
+    const data = await res.json();
+    posts = data.feed.entry || [];
+
+    render(posts);
+
+  }catch(e){
+    document.getElementById("grid").innerHTML =
+    "<p style='text-align:center'>Failed to load posts</p>";
+  }
+}
+
+/* RENDER */
+function render(items){
+
+  const grid = document.getElementById("grid");
+  grid.innerHTML = "";
+
+  items.forEach((p,i)=>{
+
+    const title = p.title.$t;
+    const link = p.link.find(l=>l.rel==="alternate").href;
+
+    let img = "https://via.placeholder.com/400";
+
+    try {
+      let content = p.content?.$t;
+
+      let match = content?.match(/<img[^>]+src="([^">]+)"/);
+
+      if (match && match[1]) {
+        img = match[1];
+      }
+
+    } catch(e) {}
+
+    grid.innerHTML += `
+      <a href="${link}" class="card">
+
+        <div class="img-box">
+          <img src="${img}" loading="lazy"/>
+        </div>
+
+        <div class="content">
+          <div class="label">Blogpost</div>
+
+          <div class="title">${title}</div>
+
+          <div class="read-more-btn" onclick="location.href='${link}'">
+            Read More →
+          </div>
+        </div>
+
+      </a>
+    `;
+  });
+}
+
+loadPosts();
